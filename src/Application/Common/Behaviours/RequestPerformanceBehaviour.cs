e6b241a3dd8a2f794191de2dd367535da818c282
Nachyn <nachyn@outlook.com>
2020-03-08 19:24:09 +0700

    Configured Application

---
+++ src/Application/Common/Behaviours/RequestPerformanceBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Common.Behaviours
+{
+    public class RequestPerformanceBehaviour<TRequest, TResponse>
+        : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        private readonly Stopwatch _timer;
+
+        public RequestPerformanceBehaviour(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+            _timer = new Stopwatch();
+        }
+
+        public async Task<TResponse> Handle(TRequest request
+            , CancellationToken cancellationToken
+            , RequestHandlerDelegate<TResponse> next)
+        {
+            _timer.Start();
+
+            var response = await next();
+
+            var elapsedMilliseconds = _timer.ElapsedMilliseconds;
+
+            if (elapsedMilliseconds > 500)
+            {
+                _logger.LogWarning(
+                    "Long Running Request: ({ElapsedMilliseconds} milliseconds) {@Request}"
+                    , elapsedMilliseconds
+                    , request);
+            }
+
+            return response;
+        }
+    }
+}
---
+++ src/Application/Common/Extensions/QueryableExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Application.Common.Extensions
+{
+    public static class QueryableExtensions
+    {
+        public static IOrderedQueryable<T> AppendOrderBy<T, TKey>(
+            this IQueryable<T> query, Expression<Func<T, TKey>> keySelector)
+        {
+            return query.Expression.Type == typeof(IOrderedQueryable<T>)
+                ? ((IOrderedQueryable<T>) query).ThenBy(keySelector)
+                : query.OrderBy(keySelector);
+        }
+
+        public static IOrderedQueryable<T> AppendOrderByDescending<T, TKey>(
+            this IQueryable<T> query, Expression<Func<T, TKey>> keySelector)
+        {
+            return query.Expression.Type == typeof(IOrderedQueryable<T>)
+                ? ((IOrderedQueryable<T>) query).ThenByDescending(keySelector)
+                : query.OrderByDescending(keySelector);
+        }
+
+        public static IOrderedEnumerable<T> AppendOrderBy<T, TKey>(
+            this IEnumerable<T> enumerable, Func<T, TKey> keySelector)
+        {
+            return enumerable.GetType() == typeof(IOrderedEnumerable<T>)
+                ? ((IOrderedEnumerable<T>) enumerable).ThenBy(keySelector)
+                : enumerable.OrderBy(keySelector);
+        }
+
+        public static IOrderedEnumerable<T> AppendOrderByDescending<T, TKey>(
+            this IEnumerable<T> enumerable, Func<T, TKey> keySelector)
+        {
+            return enumerable.GetType() == typeof(IOrderedEnumerable<T>)
+                ? ((IOrderedEnumerable<T>) enumerable).ThenByDescending(keySelector)
+                : enumerable.OrderByDescending(keySelector);
+        }
+    }
+}
