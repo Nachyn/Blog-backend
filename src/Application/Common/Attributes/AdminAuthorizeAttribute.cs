dc79547b22b80ff0212195852ba4411ce0bb083a
Nachyn <nachyn@outlook.com>
2020-03-09 02:39:22 +0700

    Added role attributes

---
+++ src/Application/Common/Attributes/AdminAuthorizeAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using UserRoles = Domain.Enums.Roles;
+
+namespace Application.Common.Attributes
+{
+    public class AdminAuthorizeAttribute : AuthorizeAttribute
+    {
+        public AdminAuthorizeAttribute()
+        {
+            Roles = UserRoles.Admin.GetEnumDescription();
+        }
+    }
+}
