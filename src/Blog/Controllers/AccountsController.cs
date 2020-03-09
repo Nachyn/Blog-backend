cd4e8c2fd68bc2f544fca2ac830233da04fde029
Nachyn <nachyn@outlook.com>
2020-03-10 01:27:11 +0700

    Added AccountsController

---
+++ src/Blog/Controllers/AccountsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Application.Accounts.Commands.CreateAccount;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    [Route("api/account")]
+    public class AccountsController : ApiController
+    {
+        [HttpPost]
+        public async Task<ActionResult<CreateAccountUserInfo>> CreateAccount(
+            [FromBody] CreateAccountCommand command)
+        {
+            return await Mediator.Send(command);
+        }
+    }
+}
