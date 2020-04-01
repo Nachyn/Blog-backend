479cd013e378c26f9d3a4536ed54ff7fe834e89f
Nachyn <nachyn@outlook.com>
2020-04-02 00:23:02 +0700

    Added common File, Photo validator

---
+++ src/Application/Common/Validators/FileValidator.cs
@@ -0,0 +1,51 @@
+﻿using Application.Common.AppSettingHelpers.Main;
+using FluentValidation;
+using FluentValidation.Validators;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
+
+namespace Application.Common.Validators
+{
+    public class FileValidator : AbstractValidator<IFormFile>
+    {
+        private readonly FileSettings _fileSettings;
+
+        private readonly IStringLocalizer<CommonValidatorsResource> _commonLocalizer;
+
+        public FileValidator(IOptions<FileSettings> fileSettings
+            , IStringLocalizer<CommonValidatorsResource> commonLocalizer)
+        {
+            _fileSettings = fileSettings.Value;
+            _commonLocalizer = commonLocalizer;
+
+            RuleFor(v => v)
+                .Custom(BeValidUploadedFile);
+        }
+
+        public void BeValidUploadedFile(IFormFile uploadedFile
+            , CustomContext context)
+        {
+            if (uploadedFile == null)
+            {
+                context.AddFailure(_commonLocalizer["FileNull"]);
+                return;
+            }
+
+            if (uploadedFile.Length > _fileSettings.MaxLengthBytes)
+            {
+                context.AddFailure(_commonLocalizer["FileMaxLength"
+                    , _fileSettings.MaxLengthBytes / 1048576]);
+
+                return;
+            }
+
+            if (uploadedFile.FileName.Length > _fileSettings.FileNameMaxLength)
+            {
+                context.AddFailure(_commonLocalizer["FileNameMaxLength"
+                    , _fileSettings.FileNameMaxLength
+                    , uploadedFile.FileName]);
+            }
+        }
+    }
+}
---
+++ src/Application/Common/Validators/PhotoValidator.cs
@@ -0,0 +1,57 @@
+﻿using Application.Common.AppSettingHelpers.Main;
+using FluentValidation;
+using FluentValidation.Validators;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
+
+namespace Application.Common.Validators
+{
+    public class PhotoValidator : AbstractValidator<IFormFile>
+    {
+        private readonly PhotoSettings _photoSettings;
+
+        private readonly IStringLocalizer<CommonValidatorsResource> _commonLocalizer;
+
+        public PhotoValidator(IOptions<PhotoSettings> photoSettings
+            , IStringLocalizer<CommonValidatorsResource> commonLocalizer)
+        {
+            _photoSettings = photoSettings.Value;
+            _commonLocalizer = commonLocalizer;
+
+            RuleFor(v => v)
+                .Custom(BeValidUploadedPhoto);
+        }
+
+        public void BeValidUploadedPhoto(IFormFile uploadedPhoto
+            , CustomContext context)
+        {
+            if (uploadedPhoto == null)
+            {
+                context.AddFailure(_commonLocalizer["PhotoNull"]);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(uploadedPhoto.ContentType)
+                || !_photoSettings.MimeContentTypes.Contains(uploadedPhoto
+                    .ContentType))
+            {
+                context.AddFailure(_commonLocalizer["PhotoErrorType"]);
+                return;
+            }
+
+            if (uploadedPhoto.Length > _photoSettings.MaxLengthBytes)
+            {
+                context.AddFailure(_commonLocalizer["PhotoMaxLength"
+                    , _photoSettings.MaxLengthBytes / 1048576]);
+            }
+
+            if (uploadedPhoto.FileName.Length > _photoSettings.FileNameMaxLength)
+            {
+                context.AddFailure(_commonLocalizer["FileNameMaxLength"
+                    , _photoSettings.FileNameMaxLength
+                    , uploadedPhoto.FileName]);
+            }
+        }
+    }
+}
