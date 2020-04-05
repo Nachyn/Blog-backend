7aa8cacefa8cbc635272153f38024bf01a53ce84
Nachyn Duuza <nachyn@outlook.com>
2020-04-05 20:16:17 +0700

    Added IdRange,Pagination validators

---
+++ src/Application/Common/Validators/IdRangeValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Localization;
+
+namespace Application.Common.Validators
+{
+    public class IdRangeValidator : AbstractValidator<int>
+    {
+        public IdRangeValidator(IStringLocalizer<CommonValidatorsResource> commonLocalizer)
+        {
+            RuleFor(v => v)
+                .Must(id => id > 0)
+                .WithMessage(commonLocalizer["IdRange"]);
+        }
+    }
+}
---
+++ src/Application/Common/Validators/PaginationValidator.cs
@@ -0,0 +1,20 @@
+﻿using Application.Common.Dtos;
+using FluentValidation;
+using Microsoft.Extensions.Localization;
+
+namespace Application.Common.Validators
+{
+    public class PaginationValidator : AbstractValidator<PaginationRequestDto>
+    {
+        public PaginationValidator(IStringLocalizer<CommonValidatorsResource> commonLocalizer)
+        {
+            RuleFor(v => v.NumberPage)
+                .Must(n => n > 0)
+                .WithMessage(commonLocalizer["NumberPageRange"]);
+
+            RuleFor(v => v.PageSize)
+                .Must(s => s > 0)
+                .WithMessage(commonLocalizer["PageSizeRange"]);
+        }
+    }
+}
