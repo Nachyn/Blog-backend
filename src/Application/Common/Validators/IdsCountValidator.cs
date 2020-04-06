4811b22dcd83f6207dc5e8bec8fd6a643bacc5e6
Nachyn Duuza <nachyn@outlook.com>
2020-04-06 17:15:18 +0700

    Added IdsCountValidator

---
+++ src/Application/Common/Validators/IdsCountValidator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Application.Common.Extensions;
+using FluentValidation.Validators;
+using Microsoft.Extensions.Localization;
+
+namespace Application.Common.Validators
+{
+    public class IdsCountValidator : PropertyValidator
+    {
+        public IdsCountValidator(IStringLocalizer<CommonValidatorsResource> commonLocalizer)
+            : base(commonLocalizer["IdsEmpty"])
+        {
+        }
+
+        protected override bool IsValid(PropertyValidatorContext context)
+        {
+            var list = (IEnumerable<int>) context.PropertyValue;
+            return !list.IsNullOrEmpty();
+        }
+    }
+}
