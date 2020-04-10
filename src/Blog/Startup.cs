7c73fb95fe671373ee4c24fe0c58e086c235e586
Nachyn Duuza <nachyn@outlook.com>
2020-04-11 01:44:38 +0700

    Added SuppressModelStateInvalidFilter

--- src/Blog/Startup.cs
+++ src/Blog/Startup.cs
@@ -46,6 +46,9 @@ namespace Blog
                         new StringEnumConverter());
                 });
 
+            services.Configure<ApiBehaviorOptions>(options =>
+                options.SuppressModelStateInvalidFilter = true);
+
             services.AddInfrastructure(Configuration);
             services.AddApplication(Configuration, WebHostEnvironment.ContentRootPath);
             services.AddAspIdentityJwt(Configuration);
