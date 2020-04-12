75e73bba0edfc0d5bed670bdc4cbd36cfa93cebb
Nachyn Duuza <nachyn@outlook.com>
2020-04-12 21:25:31 +0700

    Added AppDbContextFactory

---
+++ tests/Application.UnitTests/AppDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Application.Common.Interfaces;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UnitTests
+{
+    public static class AppDbContextFactory
+    {
+        public static AppDbContext Create(IDateTime dateTime)
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new AppDbContext(options, dateTime);
+            context.Database.EnsureCreated();
+
+            return context;
+        }
+
+        public static void Destroy(AppDbContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Dispose();
+        }
+    }
+}
