3bdab6e5a09fc235f264a5a2f8935ab1dd81374a
Nachyn Duuza <nachyn@outlook.com>
2020-04-13 20:56:12 +0700

    Added SendRestoreCodeTests

---
+++ tests/Application.UnitTests/Accounts/Commands/SendRestoreCode/SendRestoreCodeCommandTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Accounts.Commands.SendRestoreCode;
+using NUnit.Framework;
+
+namespace Application.UnitTests.Accounts.Commands.SendRestoreCode
+{
+    public class SendRestoreCodeCommandTests : AccountsTestBase
+    {
+        private SendRestoreCodeCommand.SendRestoreCodeCommandHandler GetNewHandler()
+        {
+            return new SendRestoreCodeCommand.SendRestoreCodeCommandHandler(EmailService
+                , UserManager
+                , AccountLocalizer);
+        }
+
+        [Test]
+        public async Task Handle_ShouldBeSendCode()
+        {
+            var command = new SendRestoreCodeCommand {Email = DefaultUserEmail};
+            var handler = GetNewHandler();
+
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            Assert.That(result.Info, IsNotNullOrEmpty);
+
+            command.Email = "nonexistentUser@mail.org";
+
+            result = await handler.Handle(command, CancellationToken.None);
+
+            Assert.That(result.Info, IsNotNullOrEmpty);
+        }
+    }
+}
---
+++ tests/Application.UnitTests/Accounts/Commands/SendRestoreCode/SendRestoreCodeCommandValidatorTests.cs
@@ -0,0 +1,40 @@
+﻿using Application.Accounts.Commands.SendRestoreCode;
+using Application.Common.Validators;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+
+namespace Application.UnitTests.Accounts.Commands.SendRestoreCode
+{
+    public class SendRestoreCodeCommandValidatorTests : AccountsTestBase
+    {
+        private SendRestoreCodeCommandValidator GetNewValidator()
+        {
+            return new SendRestoreCodeCommandValidator(new EmailValidator(AppUserSettingOptions
+                , CommonLocalizer));
+        }
+
+        [Test]
+        public void IsValid_ShouldBeTrue()
+        {
+            var command = new SendRestoreCodeCommand {Email = DefaultUserEmail};
+
+            var validator = GetNewValidator();
+
+            var result = validator.Validate(command);
+
+            Assert.That(result.IsValid);
+        }
+
+        [Test]
+        public void IsValid_ShouldBeFalse()
+        {
+            var validator = GetNewValidator();
+
+            validator.ShouldHaveValidationErrorFor(v => v.Email
+                , new SendRestoreCodeCommand {Email = string.Empty});
+
+            validator.ShouldHaveValidationErrorFor(v => v.Email
+                , new SendRestoreCodeCommand {Email = "e@mail"});
+        }
+    }
+}
