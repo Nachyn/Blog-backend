4c01d6f324d856d5f12218dd2b72132c353aa8ef
Nachyn Duuza <nachyn@outlook.com>
2020-04-13 21:29:33 +0700

    Added ConfirmRestoreCode Tests

---
+++ tests/Application.UnitTests/Accounts/Commands/ConfirmRestoreCode/ConfirmRestoreCodeCommandTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Accounts.Commands.ConfirmRestoreCode;
+using Application.Common.Exceptions;
+using NUnit.Framework;
+
+namespace Application.UnitTests.Accounts.Commands.ConfirmRestoreCode
+{
+    public class ConfirmRestoreCodeCommandTests : AccountsTestBase
+    {
+        private ConfirmRestoreCodeCommand.ConfirmRestoreCodeCommandHandler GetNewHandler()
+        {
+            return new ConfirmRestoreCodeCommand.ConfirmRestoreCodeCommandHandler(UserManager
+                , AccountLocalizer);
+        }
+
+        [Test]
+        public async Task Handle_ShouldGetSuccessInfo()
+        {
+            var user = Context.Users.First();
+            var code = await UserManager.GeneratePasswordResetTokenAsync(user);
+
+            var command = new ConfirmRestoreCodeCommand
+            {
+                Email = user.Email,
+                Code = code,
+                Password = "newPass123"
+            };
+
+            var handler = GetNewHandler();
+
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            Assert.That(result.Info, IsNotNullOrEmpty);
+        }
+
+        [Test]
+        public void Handle_GivenNonexistentEmail_ThrowsException()
+        {
+            var command = new ConfirmRestoreCodeCommand
+            {
+                Email = "NonexistentEmail@email.org",
+                Code = "12345678",
+                Password = "newPass123"
+            };
+
+            var handler = GetNewHandler();
+
+            Assert.ThrowsAsync<ValidationException>(async () =>
+                await handler.Handle(command, CancellationToken.None));
+        }
+
+        [Test]
+        public void Handle_GivenInvalidCodeAndPassword_ThrowsException()
+        {
+            var command = new ConfirmRestoreCodeCommand
+            {
+                Email = DefaultUserEmail,
+                Code = Guid.NewGuid().ToString(),
+                Password = string.Empty
+            };
+
+            var handler = GetNewHandler();
+
+            Assert.ThrowsAsync<ValidationException>(async () =>
+                await handler.Handle(command, CancellationToken.None));
+        }
+    }
+}
---
+++ tests/Application.UnitTests/Accounts/Commands/ConfirmRestoreCode/ConfirmRestoreCodeCommandValidatorTests.cs
@@ -0,0 +1,52 @@
+﻿using Application.Accounts.Commands.ConfirmRestoreCode;
+using Application.Common.Validators;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+
+namespace Application.UnitTests.Accounts.Commands.ConfirmRestoreCode
+{
+    public class ConfirmRestoreCodeCommandValidatorTests : AccountsTestBase
+    {
+        private ConfirmRestoreCodeCommandValidator GetNewValidator()
+        {
+            return new ConfirmRestoreCodeCommandValidator(new EmailValidator(AppUserSettingOptions
+                    , CommonLocalizer)
+                , AccountLocalizer);
+        }
+
+        [Test]
+        public void IsValid_ShouldBeTrue()
+        {
+            var command = new ConfirmRestoreCodeCommand
+            {
+                Code = "177602",
+                Email = DefaultUserEmail,
+                Password = DefaultUserPassword
+            };
+
+            var validator = GetNewValidator();
+
+            var result = validator.Validate(command);
+
+            Assert.That(result.IsValid);
+        }
+
+        [Test]
+        public void IsValid_ShouldHaveValidationError()
+        {
+            var validator = GetNewValidator();
+
+            validator.ShouldHaveValidationErrorFor(v => v.Password
+                , new ConfirmRestoreCodeCommand {Password = string.Empty});
+
+            validator.ShouldHaveValidationErrorFor(v => v.Email
+                , new ConfirmRestoreCodeCommand {Email = "admin@adil"});
+
+            validator.ShouldHaveValidationErrorFor(v => v.Email
+                , new ConfirmRestoreCodeCommand {Email = string.Empty});
+
+            validator.ShouldHaveValidationErrorFor(v => v.Code
+                , new ConfirmRestoreCodeCommand {Code = string.Empty});
+        }
+    }
+}
