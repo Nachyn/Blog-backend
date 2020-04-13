3aa3d81dd329b0a14962523dfd61e39fb3c6e346
Nachyn Duuza <nachyn@outlook.com>
2020-04-13 20:11:47 +0700

    Added Assert to CreateAccountCommandTests

--- tests/Application.UnitTests/Accounts/Commands/CreateAccount/CreateAccountCommandTests.cs
+++ tests/Application.UnitTests/Accounts/Commands/CreateAccount/CreateAccountCommandTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Application.Accounts.Commands.CreateAccount;
 using Application.Common.Exceptions;
@@ -31,6 +32,7 @@ namespace Application.UnitTests.Accounts.Commands.CreateAccount
             Assert.AreEqual(command.Username, result.Username);
             Assert.That(result.Id > DefaultUserId);
             Assert.That(result.Roles, IsNotNullOrEmpty);
+            Assert.That(Context.Users.Any(u => u.Id == result.Id));
         }
 
         [Test]
