fef2a4104cbe2526093797728a7603b9cc51527b
Nachyn Duuza <nachyn@outlook.com>
2020-04-13 18:42:09 +0700

    Added TestHelpers.MockLocalizer

---
+++ tests/Application.UnitTests/TestHelpers.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Localization;
+using NSubstitute;
+
+namespace Application.UnitTests
+{
+    public static class TestHelpers
+    {
+        public static IStringLocalizer<T> MockLocalizer<T>()
+        {
+            var localizer = Substitute.For<IStringLocalizer<T>>();
+
+            localizer[default].ReturnsForAnyArgs(info =>
+                new LocalizedString(info.Arg<string>(), info.Arg<string>()));
+
+            localizer[default, default].ReturnsForAnyArgs(info =>
+                new LocalizedString(info.Arg<string>(), info.Arg<string>()));
+
+            return localizer;
+        }
+    }
+}
