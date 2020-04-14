cb3372e1720c55a8a6e6d2d7c8e8cbd09f8120bb
Nachyn Duuza <nachyn@outlook.com>
2020-04-14 20:13:34 +0700

    Added GetUserInfo Tests

---
+++ tests/Application.UnitTests/Users/Queries/GetUserInfo/GetUserInfoQueryTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Users.Queries.GetUserInfo;
+using NUnit.Framework;
+
+namespace Application.UnitTests.Users.Queries.GetUserInfo
+{
+    public class GetUserInfoQueryTests : UsersTestBase
+    {
+        private GetUserInfoQuery.GetUserInfoQueryHandler GetNewHandler()
+        {
+            return new GetUserInfoQuery.GetUserInfoQueryHandler(Context
+                , UserLocalizer
+                , Mapper);
+        }
+
+        [Test]
+        public async Task Handle_ShouldBeGetUserInfo()
+        {
+            var query = new GetUserInfoQuery
+            {
+                UserId = DefaultUserId
+            };
+
+            var handler = GetNewHandler();
+
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            Assert.AreEqual(DefaultUserUsername, result.UserName);
+        }
+
+        [Test]
+        public void Handle_GivenInvalidId_ThrowsException()
+        {
+            var query = new GetUserInfoQuery
+            {
+                UserId = 10007
+            };
+
+            var handler = GetNewHandler();
+
+            Assert.ThrowsAsync<NotFoundException>(async () =>
+                await handler.Handle(query, CancellationToken.None));
+        }
+    }
+}
---
+++ tests/Application.UnitTests/Users/Queries/GetUserInfo/GetUserInfoQueryValidatorTests.cs
@@ -0,0 +1,33 @@
+﻿using Application.Common.Validators;
+using Application.Users.Queries.GetUserInfo;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+
+namespace Application.UnitTests.Users.Queries.GetUserInfo
+{
+    public class GetUserInfoQueryValidatorTests : UsersTestBase
+    {
+        private GetUserInfoQueryValidator GetNewValidator()
+        {
+            return new GetUserInfoQueryValidator(new IdRangeValidator(CommonLocalizer));
+        }
+
+        [Test]
+        public void IsValid_ShouldNotHaveValidationError()
+        {
+            var validator = GetNewValidator();
+
+            validator.ShouldNotHaveValidationErrorFor(v => v.UserId
+                , new GetUserInfoQuery {UserId = 1});
+        }
+
+        [Test]
+        public void IsValid_ShouldHaveValidationError()
+        {
+            var validator = GetNewValidator();
+
+            validator.ShouldHaveValidationErrorFor(v => v.UserId
+                , new GetUserInfoQuery {UserId = 0});
+        }
+    }
+}
