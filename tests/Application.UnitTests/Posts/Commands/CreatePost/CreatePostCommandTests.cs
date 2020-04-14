dce2126b4c14d3f40fc13fc9e1aa0c937052b8f2
Nachyn Duuza <nachyn@outlook.com>
2020-04-15 01:39:10 +0700

    Added CreatePostCommand Tests

---
+++ tests/Application.UnitTests/Posts/Commands/CreatePost/CreatePostCommandTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Posts.Commands.CreatePost;
+using NUnit.Framework;
+
+namespace Application.UnitTests.Posts.Commands.CreatePost
+{
+    public class CreatePostCommandTests : PostsTestBase
+    {
+        private CreatePostCommand.CreatePostCommandHandler GetNewHandler()
+        {
+            return new CreatePostCommand.CreatePostCommandHandler(Mapper
+                , Context
+                , UserAccessor);
+        }
+
+        [Test]
+        public async Task Handle_ShouldBeCreatePost()
+        {
+            var command = new CreatePostCommand
+            {
+                Text = Guid.NewGuid().ToString()
+            };
+
+            var handler = GetNewHandler();
+
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            Assert.AreEqual(DefaultUserId, result.UserId);
+            Assert.AreEqual(command.Text, result.Text);
+            Assert.That(DateTimeService.NowUtc > result.LoadedUtc);
+            Assert.That(Context.Posts.Any(p => p.Id == result.Id));
+        }
+    }
+}
---
+++ tests/Application.UnitTests/Posts/Commands/CreatePost/CreatePostCommandValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿using Application.Posts.Commands.CreatePost;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+
+namespace Application.UnitTests.Posts.Commands.CreatePost
+{
+    public class CreatePostCommandValidatorTests : PostsTestBase
+    {
+        private CreatePostCommandValidator GetNewValidator()
+        {
+            return new CreatePostCommandValidator(PostLocalizer);
+        }
+
+        [Test]
+        public void IsValid_ShouldNotHaveValidationError()
+        {
+            var validator = GetNewValidator();
+
+            validator.ShouldNotHaveValidationErrorFor(v => v.Text
+                , new CreatePostCommand {Text = "simple text"});
+        }
+
+        [Test]
+        public void IsValid_ShouldHaveValidationError()
+        {
+            var validator = GetNewValidator();
+
+            validator.ShouldHaveValidationErrorFor(v => v.Text
+                , new CreatePostCommand {Text = "  "});
+
+            validator.ShouldHaveValidationErrorFor(v => v.Text
+                , new CreatePostCommand {Text = string.Empty});
+
+            validator.ShouldHaveValidationErrorFor(v => v.Text
+                , new CreatePostCommand {Text = null});
+        }
+    }
+}
