6dbda3dd9a9830b0daa10f202cac8049a4c4bef9
Nachyn Duuza <nachyn@outlook.com>
2020-04-14 18:42:57 +0700

    Added UsersDownloadPhoto Tests

---
+++ tests/Application.UnitTests/Users/Queries/DownloadPhoto/DownloadPhotoQueryTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Users.Queries.DownloadPhoto;
+using NUnit.Framework;
+
+namespace Application.UnitTests.Users.Queries.DownloadPhoto
+{
+    public class DownloadPhotoQueryTests : UsersTestBase
+    {
+        private DownloadPhotoQuery.DownloadPhotoQueryHandler GetNewHandler()
+        {
+            return new DownloadPhotoQuery.DownloadPhotoQueryHandler(Mapper
+                , Context
+                , UserLocalizer);
+        }
+
+        [Test]
+        public async Task Handle_ShouldBeGetPhoto()
+        {
+            var query = new DownloadPhotoQuery
+            {
+                PhotoId = DefaultPhotoIds.First()
+            };
+
+            var handler = GetNewHandler();
+
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            Assert.That(result.Name, IsNotNullOrEmpty);
+            Assert.That(result.Path, IsNotNullOrEmpty);
+        }
+
+        [Test]
+        public void Handle_GivenInvalidPhotoId_ThrowsException()
+        {
+            var query = new DownloadPhotoQuery
+            {
+                PhotoId = 10005
+            };
+
+            var handler = GetNewHandler();
+
+            Assert.ThrowsAsync<NotFoundException>(async () =>
+                await handler.Handle(query, CancellationToken.None));
+        }
+    }
+}
---
+++ tests/Application.UnitTests/Users/Queries/DownloadPhoto/DownloadPhotoQueryValidatorTests.cs
@@ -0,0 +1,36 @@
+﻿using Application.Common.Validators;
+using Application.Users.Queries.DownloadPhoto;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+
+namespace Application.UnitTests.Users.Queries.DownloadPhoto
+{
+    public class DownloadPhotoQueryValidatorTests : UsersTestBase
+    {
+        private DownloadPhotoQueryValidator GetNewValidator()
+        {
+            return new DownloadPhotoQueryValidator(new IdRangeValidator(CommonLocalizer));
+        }
+
+        [Test]
+        public void IsValid_ShouldNotHaveValidationError()
+        {
+            var validator = GetNewValidator();
+
+            validator.ShouldNotHaveValidationErrorFor(v => v.PhotoId
+                , new DownloadPhotoQuery {PhotoId = 50});
+        }
+
+        [Test]
+        public void IsValid_ShouldHaveValidationError()
+        {
+            var validator = GetNewValidator();
+
+            validator.ShouldHaveValidationErrorFor(v => v.PhotoId
+                , new DownloadPhotoQuery {PhotoId = 0});
+
+            validator.ShouldHaveValidationErrorFor(v => v.PhotoId
+                , new DownloadPhotoQuery {PhotoId = -1});
+        }
+    }
+}
