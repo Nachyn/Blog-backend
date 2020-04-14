a7d7b1aa72a95d80fe2955737c8e466277aead04
Nachyn Duuza <nachyn@outlook.com>
2020-04-15 01:05:10 +0700

    DeletePhotosCommandTests changed Asserts. Added PostsTestBase

---
+++ tests/Application.UnitTests/Posts/PostsTestBase.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Common.AppSettingHelpers.Main;
+using Application.Common.Interfaces;
+using Application.Posts;
+using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
+using NSubstitute;
+
+namespace Application.UnitTests.Posts
+{
+    public class PostsTestBase : CommonTestBase
+    {
+        protected List<int> DefaultFileIds;
+
+        protected int DefaultPostId;
+
+        protected IStringLocalizer<PostsResource> PostLocalizer;
+
+        protected IFileService FileService;
+
+        protected IOptions<FilesDirectory> FilesDirectory;
+
+        public PostsTestBase()
+        {
+            PostLocalizer = TestHelpers.MockLocalizer<PostsResource>();
+            FileService = Substitute.For<IFileService>();
+
+            FilesDirectory = Options.Create(Configuration
+                .GetSection(nameof(FilesDirectory))
+                .Get<FilesDirectory>());
+        }
+
+        public override async Task InitializeDatabase()
+        {
+            await base.InitializeDatabase();
+            SeedDefaultData();
+        }
+
+        protected List<IFormFile> CreateDefaultFormFiles()
+        {
+            var formFiles = new List<IFormFile>();
+            for (var i = 0; i < 5; i++)
+            {
+                var formFile = Substitute.For<IFormFile>();
+                formFile.FileName.Returns($"{Guid.NewGuid():N}.pdf");
+                formFile.ContentType.Returns("application/pdf");
+                formFile.Length.Returns(7000000);
+                formFiles.Add(formFile);
+            }
+
+            return formFiles;
+        }
+
+        private void SeedDefaultData()
+        {
+            var post = new Post
+            {
+                Text = Guid.NewGuid().ToString(),
+                UserId = DefaultUserId
+            };
+
+            Context.Posts.Add(post);
+            Context.SaveChanges();
+
+            var defaultFiles = new List<PostFile>();
+            for (var i = 0; i < 5; i++)
+            {
+                defaultFiles.Add(new PostFile
+                {
+                    Name = Guid.NewGuid().ToString(),
+                    Path = Guid.NewGuid().ToString(),
+                    PostId = post.Id
+                });
+            }
+
+            Context.PostFiles.AddRange(defaultFiles);
+            Context.SaveChanges();
+
+            DefaultFileIds = defaultFiles.Select(f => f.Id).ToList();
+            DefaultPostId = post.Id;
+            DetachAllEntities();
+        }
+    }
+}
--- tests/Application.UnitTests/Users/Commands/DeletePhotos/DeletePhotosCommandTests.cs
+++ tests/Application.UnitTests/Users/Commands/DeletePhotos/DeletePhotosCommandTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Users.Commands.DeletePhotos;
@@ -21,15 +22,17 @@ namespace Application.UnitTests.Users.Commands.DeletePhotos
         {
             var command = new DeletePhotosCommand
             {
-                Ids = DefaultPhotoIds
+                Ids = new List<int>()
             };
             command.Ids.AddRange(new[] {1001, 1002, 1003});
+            command.Ids.AddRange(DefaultPhotoIds);
 
             var handler = GetNewHandler();
 
             var result = await handler.Handle(command, CancellationToken.None);
 
-            Assert.That(result.Ids.All(id => DefaultPhotoIds.Contains(id)));
+            Assert.That(result.Ids, IsNotNullOrEmpty);
+            Assert.That(DefaultPhotoIds.All(id => result.Ids.Contains(id)));
         }
     }
 }
