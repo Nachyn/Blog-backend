f45f419c2295982ad76b8763df9e0e20c4661e58
Nachyn Duuza <nachyn@outlook.com>
2020-04-15 01:26:42 +0700

    Added DeletePosts Tests

---
+++ tests/Application.UnitTests/Posts/Commands/DeletePosts/DeletePostsCommandTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Posts.Commands.DeletePosts;
+using NUnit.Framework;
+
+namespace Application.UnitTests.Posts.Commands.DeletePosts
+{
+    public class DeletePostsCommandTests : PostsTestBase
+    {
+        private DeletePostsCommand.DeletePostsCommandHandler GetNewHandler()
+        {
+            return new DeletePostsCommand.DeletePostsCommandHandler(Context
+                , UserAccessor
+                , FileService
+                , FilesDirectory);
+        }
+
+        [Test]
+        public async Task Handle_ShouldBeDeletePostAndPostFiles()
+        {
+            var command = new DeletePostsCommand
+            {
+                Ids = new List<int> {DefaultPostId}
+            };
+
+            var handler = GetNewHandler();
+
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            Assert.That(result.Ids, IsNotNullOrEmpty);
+            Assert.That(result.Ids.Contains(DefaultPostId));
+            Assert.That(!Context.Posts.Any(p => p.Id == DefaultPostId));
+            Assert.That(!Context.PostFiles.Any(p => DefaultFileIds.Contains(p.Id)));
+        }
+    }
+}
---
+++ tests/Application.UnitTests/Posts/Commands/DeletePosts/DeletePostsCommandValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Application.Common.Validators;
+using Application.Posts.Commands.DeletePosts;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+
+namespace Application.UnitTests.Posts.Commands.DeletePosts
+{
+    public class DeletePostsCommandValidatorTests : PostsTestBase
+    {
+        private DeletePostsCommandValidator GetNewValidator()
+        {
+            return new DeletePostsCommandValidator(new IdsCountValidator(CommonLocalizer)
+                , new IdRangeValidator(CommonLocalizer));
+        }
+
+        [Test]
+        public void IsValid_ShouldNotHaveValidationError()
+        {
+            var validator = GetNewValidator();
+
+            validator.ShouldNotHaveValidationErrorFor(v => v.Ids
+                , new DeletePostsCommand {Ids = new List<int> {1, 2, 3, 4, 5}});
+        }
+
+        [Test]
+        public void IsValid_ShouldHaveValidationError()
+        {
+            var validator = GetNewValidator();
+
+            validator.ShouldHaveValidationErrorFor(v => v.Ids
+                , new DeletePostsCommand {Ids = new List<int>()});
+
+            validator.ShouldHaveValidationErrorFor(v => v.Ids
+                , new DeletePostsCommand {Ids = null});
+        }
+    }
+}
