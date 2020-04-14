79a8aa44b2e7a6bf141d2ff2a59d13f9039765f9
Nachyn Duuza <nachyn@outlook.com>
2020-04-15 01:05:33 +0700

    Added PostsDeleteFilesCommand Tests

---
+++ tests/Application.UnitTests/Posts/Commands/DeleteFiles/DeleteFilesCommandTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Posts.Commands.DeleteFiles;
+using NUnit.Framework;
+
+namespace Application.UnitTests.Posts.Commands.DeleteFiles
+{
+    public class DeleteFilesCommandTests : PostsTestBase
+    {
+        private DeleteFilesCommand.DeleteFilesCommandHandler GetNewHandler()
+        {
+            return new DeleteFilesCommand.DeleteFilesCommandHandler(Context
+                , UserAccessor
+                , FileService
+                , FilesDirectory
+                , PostLocalizer);
+        }
+
+        [Test]
+        public async Task Handle_ShouldBeDeleteFiles()
+        {
+            var command = new DeleteFilesCommand
+            {
+                PostId = DefaultPostId,
+                Ids = DefaultFileIds
+            };
+
+            var handler = GetNewHandler();
+
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            Assert.That(result.Ids, IsNotNullOrEmpty);
+            Assert.That(command.Ids.All(id => result.Ids.Contains(id)));
+        }
+
+        [Test]
+        public void Handle_GivenInvalidPostId_ThrowsException()
+        {
+            var command = new DeleteFilesCommand
+            {
+                PostId = 10005,
+                Ids = DefaultFileIds
+            };
+
+            var handler = GetNewHandler();
+
+            Assert.ThrowsAsync<ValidationException>(async () =>
+                await handler.Handle(command, CancellationToken.None));
+        }
+    }
+}
---
+++ tests/Application.UnitTests/Posts/Commands/DeleteFiles/DeleteFilesCommandValidatorTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Application.Common.Validators;
+using Application.Posts.Commands.DeleteFiles;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+
+namespace Application.UnitTests.Posts.Commands.DeleteFiles
+{
+    public class DeleteFilesCommandValidatorTests : PostsTestBase
+    {
+        private DeleteFilesCommandValidator GetNewValidator()
+        {
+            return new DeleteFilesCommandValidator(new IdsCountValidator(CommonLocalizer)
+                , new IdRangeValidator(CommonLocalizer));
+        }
+
+        [Test]
+        public void IsValid_ShouldNotHaveValidationError()
+        {
+            var validator = GetNewValidator();
+
+            validator.ShouldNotHaveValidationErrorFor(v => v.Ids
+                , new DeleteFilesCommand {Ids = new List<int> {1, 2, 3, 4, 5}});
+
+            validator.ShouldNotHaveValidationErrorFor(v => v.PostId
+                , new DeleteFilesCommand {PostId = 1});
+        }
+
+        [Test]
+        public void IsValid_ShouldHaveValidationError()
+        {
+            var validator = GetNewValidator();
+
+            validator.ShouldHaveValidationErrorFor(v => v.Ids
+                , new DeleteFilesCommand {Ids = new List<int>()});
+
+            validator.ShouldHaveValidationErrorFor(v => v.Ids
+                , new DeleteFilesCommand {Ids = null});
+
+            validator.ShouldHaveValidationErrorFor(v => v.PostId
+                , new DeleteFilesCommand {PostId = 0});
+
+            validator.ShouldHaveValidationErrorFor(v => v.PostId
+                , new DeleteFilesCommand {PostId = -1});
+        }
+    }
+}
