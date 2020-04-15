2bfc504e680aee1d9e2838aa6630b0d7bb019c3b
Nachyn Duuza <nachyn@outlook.com>
2020-04-15 17:36:14 +0700

    Added DownloadFileQuery Tests

---
+++ tests/Application.UnitTests/Posts/Queries/DownloadFile/DownloadFileQueryTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Posts.Queries.DownloadFile;
+using NUnit.Framework;
+
+namespace Application.UnitTests.Posts.Queries.DownloadFile
+{
+    public class DownloadFileQueryTests : PostsTestBase
+    {
+        private DownloadFileQuery.DownloadFileQueryHandler GetNewHandler()
+        {
+            return new DownloadFileQuery.DownloadFileQueryHandler(Context
+                , Mapper
+                , PostLocalizer);
+        }
+
+        [Test]
+        public async Task Handle_ShouldBeReturnFile()
+        {
+            var query = new DownloadFileQuery
+            {
+                FileId = DefaultFileIds.First()
+            };
+
+            var handler = GetNewHandler();
+
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            Assert.That(result.Name, IsNotNullOrEmpty);
+            Assert.That(result.Path, IsNotNullOrEmpty);
+        }
+
+        [Test]
+        public void Handle_GivenInvalidFileId_ThrowsException()
+        {
+            var query = new DownloadFileQuery
+            {
+                FileId = 1007
+            };
+
+            var handler = GetNewHandler();
+
+            Assert.ThrowsAsync<NotFoundException>(async () =>
+                await handler.Handle(query, CancellationToken.None));
+        }
+    }
+}
---
+++ tests/Application.UnitTests/Posts/Queries/DownloadFile/DownloadFileQueryValidatorTests.cs
@@ -0,0 +1,36 @@
+﻿using Application.Common.Validators;
+using Application.Posts.Queries.DownloadFile;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+
+namespace Application.UnitTests.Posts.Queries.DownloadFile
+{
+    public class DownloadFileQueryValidatorTests : PostsTestBase
+    {
+        private DownloadFileQueryValidator GetNewValidator()
+        {
+            return new DownloadFileQueryValidator(new IdRangeValidator(CommonLocalizer));
+        }
+
+        [Test]
+        public void IsValid_ShouldNotHaveValidationError()
+        {
+            var validator = GetNewValidator();
+
+            validator.ShouldNotHaveValidationErrorFor(v => v.FileId
+                , new DownloadFileQuery {FileId = 1});
+        }
+
+        [Test]
+        public void IsValid_ShouldHaveValidationError()
+        {
+            var validator = GetNewValidator();
+
+            validator.ShouldHaveValidationErrorFor(v => v.FileId
+                , new DownloadFileQuery {FileId = 0});
+
+            validator.ShouldHaveValidationErrorFor(v => v.FileId
+                , new DownloadFileQuery {FileId = -1});
+        }
+    }
+}
