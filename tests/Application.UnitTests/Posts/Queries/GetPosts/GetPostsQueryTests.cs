9b06827304f31eb21310aa6d2a4c60a4206f7a97
Nachyn Duuza <nachyn@outlook.com>
2020-04-15 18:01:44 +0700

    Added GetPostsQuery Tests

---
+++ tests/Application.UnitTests/Posts/Queries/GetPosts/GetPostsQueryTests.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Posts.Queries.GetPosts;
+using NUnit.Framework;
+
+namespace Application.UnitTests.Posts.Queries.GetPosts
+{
+    public class GetPostsQueryTests : PostsTestBase
+    {
+        private GetPostsQuery.GetPostsQueryHandler GetNewHandler()
+        {
+            return new GetPostsQuery.GetPostsQueryHandler(Mapper
+                , Context
+                , PostLocalizer);
+        }
+
+        [Test]
+        public async Task Handle_ShouldBeReturnPosts()
+        {
+            var query = new GetPostsQuery
+            {
+                NumberPage = 1,
+                PageSize = 1,
+                Sort = GetPostsPostSort.LoadedAsc,
+                UserId = DefaultUserId
+            };
+
+            var handler = GetNewHandler();
+
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            Assert.AreEqual(query.NumberPage, result.CurrentPage);
+            Assert.AreEqual(1, result.CountAllPages);
+            Assert.That(result.Posts.Count == 1);
+            var post = result.Posts.First();
+            Assert.That(post.Id > 0);
+            Assert.That(post.Files.Count == DefaultFileIds.Count);
+            Assert.That(DateTimeService.NowUtc > post.LastModifiedUtc
+                        || post.LastModifiedUtc == null);
+            Assert.That(DateTimeService.NowUtc > post.LoadedUtc);
+            Assert.That(post.Text, IsNotNullOrEmpty);
+            post.Files.ForEach(f =>
+            {
+                Assert.That(f.Id > 0);
+                Assert.That(f.Name, IsNotNullOrEmpty);
+                Assert.That(DateTimeService.NowUtc > f.LoadedUtc);
+            });
+        }
+
+        [Test]
+        public void Handle_GivenInvalidUserId_ThrowsException()
+        {
+            var query = new GetPostsQuery
+            {
+                NumberPage = 1,
+                PageSize = 1,
+                Sort = GetPostsPostSort.LoadedAsc,
+                UserId = 2020
+            };
+
+            var handler = GetNewHandler();
+
+            Assert.ThrowsAsync<ValidationException>(async () =>
+                await handler.Handle(query, CancellationToken.None));
+        }
+    }
+}
---
+++ tests/Application.UnitTests/Posts/Queries/GetPosts/GetPostsQueryValidatorTests.cs
@@ -0,0 +1,55 @@
+﻿using Application.Common.Validators;
+using Application.Posts.Queries.GetPosts;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+
+namespace Application.UnitTests.Posts.Queries.GetPosts
+{
+    public class GetPostsQueryValidatorTests : PostsTestBase
+    {
+        private GetPostsQueryValidator GetNewValidator()
+        {
+            return new GetPostsQueryValidator(new IdRangeValidator(CommonLocalizer)
+                , new PaginationValidator(CommonLocalizer));
+        }
+
+        [Test]
+        public void IsValid_ShouldNotHaveValidationError()
+        {
+            var validator = GetNewValidator();
+
+            validator.ShouldNotHaveValidationErrorFor(v => v.NumberPage
+                , new GetPostsQuery {NumberPage = 1});
+
+            validator.ShouldNotHaveValidationErrorFor(v => v.PageSize
+                , new GetPostsQuery {PageSize = 1});
+
+            validator.ShouldNotHaveValidationErrorFor(v => v.UserId
+                , new GetPostsQuery {UserId = 1});
+        }
+
+        [Test]
+        public void IsValid_ShouldHaveValidationError()
+        {
+            var validator = GetNewValidator();
+
+            validator.ShouldHaveValidationErrorFor(v => v.NumberPage
+                , new GetPostsQuery {NumberPage = 0});
+
+            validator.ShouldHaveValidationErrorFor(v => v.NumberPage
+                , new GetPostsQuery {NumberPage = -1});
+
+            validator.ShouldHaveValidationErrorFor(v => v.PageSize
+                , new GetPostsQuery {PageSize = 0});
+
+            validator.ShouldHaveValidationErrorFor(v => v.PageSize
+                , new GetPostsQuery {PageSize = -1});
+
+            validator.ShouldHaveValidationErrorFor(v => v.UserId
+                , new GetPostsQuery {UserId = 0});
+
+            validator.ShouldHaveValidationErrorFor(v => v.UserId
+                , new GetPostsQuery {UserId = -1});
+        }
+    }
+}
