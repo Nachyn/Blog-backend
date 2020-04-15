9620084f03aef1e731abad0d581954389b091d3f
Nachyn Duuza <nachyn@outlook.com>
2020-04-15 17:26:33 +0700

    Added UpdatePostCommand Tests

---
+++ tests/Application.UnitTests/Posts/Commands/UpdatePost/UpdatePostCommandTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Posts.Commands.UpdatePost;
+using NUnit.Framework;
+
+namespace Application.UnitTests.Posts.Commands.UpdatePost
+{
+    public class UpdatePostCommandTests : PostsTestBase
+    {
+        private UpdatePostCommand.UpdatePostCommandHandler GetNewHandler()
+        {
+            return new UpdatePostCommand.UpdatePostCommandHandler(Mapper
+                , PostLocalizer
+                , Context
+                , UserAccessor);
+        }
+
+        [Test]
+        public async Task Handle_ShouldBeUpdatePost()
+        {
+            var command = new UpdatePostCommand
+            {
+                PostId = DefaultPostId,
+                Text = Guid.NewGuid().ToString()
+            };
+
+            var handler = GetNewHandler();
+
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            Assert.AreEqual(command.Text, result.Text);
+            Assert.That(DateTimeService.NowUtc > result.LastModifiedUtc);
+        }
+
+        [Test]
+        public void Handle_GivenInvalidPostId_ThrowsException()
+        {
+            var command = new UpdatePostCommand
+            {
+                PostId = 1004,
+                Text = Guid.NewGuid().ToString()
+            };
+
+            var handler = GetNewHandler();
+
+            Assert.ThrowsAsync<ValidationException>(async () =>
+                await handler.Handle(command, CancellationToken.None));
+        }
+    }
+}
---
+++ tests/Application.UnitTests/Posts/Commands/UpdatePost/UpdatePostCommandValidatorTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Application.Common.Validators;
+using Application.Posts.Commands.UpdatePost;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+
+namespace Application.UnitTests.Posts.Commands.UpdatePost
+{
+    public class UpdatePostCommandValidatorTests : PostsTestBase
+    {
+        private UpdatePostCommandValidator GetNewValidator()
+        {
+            return new UpdatePostCommandValidator(new IdRangeValidator(CommonLocalizer)
+                , PostLocalizer);
+        }
+
+        [Test]
+        public void IsValid_ShouldNotHaveValidationError()
+        {
+            var validator = GetNewValidator();
+
+            validator.ShouldNotHaveValidationErrorFor(v => v.PostId
+                , new UpdatePostCommand {PostId = 1});
+
+            validator.ShouldNotHaveValidationErrorFor(v => v.Text
+                , new UpdatePostCommand {Text = Guid.NewGuid().ToString()});
+        }
+
+        [Test]
+        public void IsValid_ShouldHaveValidationError()
+        {
+            var validator = GetNewValidator();
+
+            validator.ShouldHaveValidationErrorFor(v => v.PostId
+                , new UpdatePostCommand {PostId = 0});
+
+            validator.ShouldHaveValidationErrorFor(v => v.PostId
+                , new UpdatePostCommand {PostId = -1});
+
+            validator.ShouldHaveValidationErrorFor(v => v.Text
+                , new UpdatePostCommand {Text = "  "});
+
+            validator.ShouldHaveValidationErrorFor(v => v.Text
+                , new UpdatePostCommand {Text = null});
+        }
+    }
+}
